06e063aad0cbcc20e13d7b5025b27e07a09e7751
elroy404 <sdzgrail@gmail.com>
2016-08-03 15:33:56 -0700

    8/3 v1

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var quiz = [
+  {
+    question: 'Who is master of their domain?',
+    answers: [
+      'Jerry',
+      'Elaine',
+      'George',
+      'Kramer'
+    ],
+    correctAnswer: 0
+  },
+  {
+    question: 'What type of desert got Jerry sick?',
+    answers: [
+      'Jerry',
+      'Elaine',
+      'George',
+      'Kramer'
+    ],
+    correctAnswer: 0
+  },
